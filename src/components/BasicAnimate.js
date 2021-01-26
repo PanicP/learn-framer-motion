7826a629fb236d083e222f68edcde560277cdd9a
PanicP <filmous2136@gmail.com>
2021-01-26 23:50:16 +0700

    WIP: Test Basic animate with reset button

--- src/components/BasicAnimate.js
+++ src/components/BasicAnimate.js
@@ -1,8 +1,7 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export const BasicAnimate = () => (
-  <motion.div
-    animate={{ scale: 2 }}
-    transition={{ duration: 0.5 }}
-  />
-)
+  <div className="basic-animate">
+    <motion.div animate={{ scale: 2 }} transition={{ duration: 0.5 }} />
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { useState } from "react";
-import { render } from "react-dom"
-import { BasicAnimate } from "./components/BasicAnimate"
-import { Refresh } from "./components/Refresh"
-
+import { render } from "react-dom";
+import { Route, Router, Switch } from "react-router-dom";
+import { BasicAnimate } from "./components/BasicAnimate";
+import { Refresh } from "./components/Refresh";
+import { history } from "./history";
 
 import "./index.css";
 
@@ -11,10 +12,19 @@ const App = () => {
   const [count, setCount] = useState(0);
   return (
     <>
-      <Refresh onClick={() => setCount(count + 1)} />
-      <div className="example-container">
-        <BasicAnimate key={count} />
-      </div>
+      <Router history={history}>
+        <>
+          <Refresh onClick={() => setCount(count + 1)} />
+          <Switch>
+            <Route
+              path="/basic-animate"
+              exact
+              render={() => <BasicAnimate key={`basic-animate-${count}`} />}
+            />
+            <Route path="/" component={() => <div>this is fallback</div>} />
+          </Switch>
+        </>
+      </Router>
     </>
   );
 };
